2502240a81a518699eaf4128901093d5bf388eb2
Megan O'Keefe <megan037@gmail.com>
2020-03-07 09:29:24 -0500

    upgrades hub register cmd

--- kops-gce/connect-hub.sh
+++ kops-gce/connect-hub.sh
@@ -58,7 +58,7 @@ fi
 
 
 ### Connect Cluster to Hub
-gcloud beta container memberships register $ONPREM_CLUSTER_NAME_BASE\
+gcloud container hub memberships register $ONPREM_CLUSTER_NAME_BASE\
  --context=$ONPREM_CLUSTER_NAME \
  --service-account-key-file=$GKE_SA_CREDS \
  --kubeconfig=$ONPREM_KUBECONFIG \
